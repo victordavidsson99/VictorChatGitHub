5f52aca5524e1ca8edc16fcea214330659888b39
Victor Davidsson <victor.davidsson@learnet.se>
2018-01-15 13:09:17 +0100

    While error sak

--- VictorChatGitHub/Program.cs
+++ VictorChatGitHub/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VictorChatGitHub
 {
+
+    //Av Victor Davidsson 2018-01-15
+    //Utifrån http://csharpskolan.se/article/broadcast-och-chatt
+
     class Program
     {
+
+        private const int ListenPort = 11000;
+
         static void Main(string[] args)
         {
+        }
+
+        static void Listener()
+        {
+            UdpClient listener = new UdpClient(ListenPort);
+
+            try
+            {
+                while (true)
+                {
+
+                    IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, ListenPort);
+                    byte[] bytes = listener.Receive(ref groupEP);
+                    Console.WriteLine("Received broadcast from {0} : {1}\n", groupEP.ToString(), Encoding.UTF8.GetString(bytes, 0, bytes.Length));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                listener.Close()
+            }
+
         }
     }
 }
